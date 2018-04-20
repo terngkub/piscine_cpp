df7c047f818c4357555cf7f988a77646f722ee0f
terngkub <terngnattapol@gmail.com>
2018-04-20 14:37:32 +0200

    finish day00/ex00

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkamolba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/20 14:27:18 by nkamolba          #+#    #+#             */
+/*   Updated: 2018/04/20 14:36:00 by nkamolba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+					std::cout << (char)(argv[i][j] - 32);
+				else 
+					std::cout << argv[i][j];
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}

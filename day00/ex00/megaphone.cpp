9077bac0453c1319657b6eff0a728917fe2b0440
terngkub <terngnattapol@gmail.com>
2018-04-20 15:35:16 +0200

    improve day00/ex00: use toupper

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: nkamolba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 14:27:18 by nkamolba          #+#    #+#             */
-/*   Updated: 2018/04/20 14:36:00 by nkamolba         ###   ########.fr       */
+/*   Updated: 2018/04/20 15:34:47 by nkamolba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int argc, char **argv)
 {
 	int	i;
 	int	j;
+
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
@@ -25,13 +26,7 @@ int	main(int argc, char **argv)
 		{
 			j = 0;
 			while (argv[i][j])
-			{
-				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-					std::cout << (char)(argv[i][j] - 32);
-				else 
-					std::cout << argv[i][j];
-				j++;
-			}
+				std::cout << (char)toupper(argv[i][j++]);
 			i++;
 		}
 		std::cout << std::endl;
